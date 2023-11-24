fed972663551d4552251c90f22ac6e0bcf157356
rreyes5132 <rreyes5132@ceuandalucia.es>
2023-11-24 19:25:33 +0100

    Subo proyecto

--- Viernes/src/Nuevoproyec.java
+++ Viernes/src/Nuevoproyec.java
@@ -4,6 +4,8 @@ public class Nuevoproyec {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Mi nombre es Rocío");
+		System.out.println("estoy desarrollando");
+		
 	}
 
 }
